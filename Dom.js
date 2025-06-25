f82ee09ffe03fd7117adccbbe937bc65b1e6238f
varshith45555 <varshithgummadi2005@gmail.com>
2025-06-25 15:28:36 +0530

    Add files via upload

---
+++ Dom.js
@@ -0,0 +1,7 @@
+document.title ="Dom in Javascript";
+document.body.style.background = "lightblue";
+document.getElementById("Mrcet").innerHTML = "Hello,World!";
+document.getElementById("paral").innerHTML = "welcome to DOM Manipulation";
+document.getElementsByTagName("h1")[1].style.color ="red";
+document.getElementsByClassName("DS").style.color = "blue";
+document.getElementById("Mr").style.color ="yellow";
