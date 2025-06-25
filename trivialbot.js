af4812c8c327b2348b96337659114a61452e5849
varshith45555 <varshithgummadi2005@gmail.com>
2025-06-25 11:20:11 +0530

    Add files via upload

---
+++ trivialbot.js
@@ -0,0 +1,13 @@
+console.log("Hello! I'm your coding fun fact guide!");
+const botName = "Giri";
+const botLocation = "hydrebad";
+const favoriteLanguage = "js";
+console.log("My name is " + botName +  " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+let codingFact = favoriteLanguage + "this language is useful" + ".";
+console.log(codingFact);
+codingFact = favoriteLanguage + "this language is used in front-end" + ".";
+console.log(codingFact);
+codingFact = favoriteLanguage + "It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".";
+console.log(codingFact);
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".")
