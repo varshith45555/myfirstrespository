de19967e49287df298874046e3477cc34c593480
varshith45555 <varshithgummadi2005@gmail.com>
2025-06-25 11:24:44 +0530

    Add files via upload

---
+++ buildingsentencemaker.js
@@ -0,0 +1,24 @@
+let adjective = "fierce";
+let noun = "dragon";
+let verb = "sleeping";
+let place = "mountain cave";
+let adjective2 = "scaly";
+let noun2 = "sheep";
+
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+
+console.log("First story: " + firstStory);
+
+
+adjective = "mysterious";
+noun = "wizard";
+verb = "casting spells";
+place = "enchanted forest";
+adjective2 = "glowing";
+noun2 = "magic potions";
+
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("Second story: " + secondStory);
