fa90a73f2405abbd8c08131bc7cd91d6689ca989
varshith45555 <varshithgummadi2005@gmail.com>
2025-06-28 12:06:29 +0530

    Add files via upload

---
+++ leapyearcalculator .js
@@ -0,0 +1,14 @@
+
+function isLeapYear(year) {
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    return `${year} is a leap year.`;
+  } else {
+    return `${year} is not a leap year.`;
+  }
+}
+
+
+const year = 2025;
+const result = isLeapYear(year);
+
+console.log(result);
